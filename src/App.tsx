f5a728042df27ce4742e734a819d62c18f69948f
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-15 23:46:35 +0400

    done reddbery logo and bg image

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+ useEffect(() => {
+   document.body.style.backgroundImage = "url('/redberyIMG.png')";
+   document.body.style.backgroundSize = "cover";
+   document.body.style.backgroundPosition = "center";
+   document.body.style.margin = "0"; 
+   document.body.style.height = "100vh";
+
+   return () => {
+     document.body.style.backgroundImage = "";
+   };
+ }, []);
+
+  return (
+    <div>
+      <div className='px-[70px]'>
+        <img className="my-[25px]" src='./logo.png' alt='redberry logo' />
+        <hr className='bg-[#1A1A1A] h-[1px] border-none' />
+      </div>
+    </div>
+  );
+}
+
+export default App;
