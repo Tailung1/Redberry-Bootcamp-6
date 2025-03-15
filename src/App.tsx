221c0a6621f27d9ea48fda5304dbe97d2254dc59
Tailung1 <ramazsamitashvili11@gmail.com>
2025-03-16 00:39:34 +0400

    done landing page

--- src/App.tsx
+++ src/App.tsx
@@ -16,15 +16,25 @@ function App() {
  }, []);
 
   return (
-    <div>
+    <div className='relative '>
       <div className='px-[70px]'>
-        <img className="my-[25px]" src='./logo.png' alt='redberry logo' />
+        <img
+          className='my-[25px]'
+          src='./logo.png'
+          alt='redberry logo'
+        />
         <hr className='bg-[#1A1A1A] h-[1px] border-none' />
       </div>
-      <div className="h-[100vh] flex flex-col  justify-center items-center">
-        <button>Click</button>
-        <img src="/logo2.png" alt="" />
+      <div className='h-[100vh] flex flex-col  justify-center items-center'>
+        <button className='bg-[#1A1A1A] rounded-[8px] font-[20px] leading-normal text-[20px] text-white px-[120px] py-[14px]'>
+          ᲠᲔᲖᲘᲣᲛᲔᲡ ᲓᲐᲛᲐᲢᲔᲑᲐ
+        </button>
       </div>
+      <img
+        className='absolute top-[47%] right-[27%]'
+        src='/logo2.png'
+        alt=''
+      />
     </div>
   );
 }
